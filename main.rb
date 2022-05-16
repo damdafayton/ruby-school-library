21fa55ee5afb5098c1c5d2a1a5d845017dd3ed13
MUNEEB UL REHMAN <bhatmuneeb.mb@gmail.com>
2022-05-16 14:56:55 +0530

    Refactored main.rb

--- main.rb
+++ main.rb
@@ -84,6 +84,39 @@ def list_rentals(app)
   rentals.each { |rental| puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" }
 end
 
+def options
+  [
+    '',
+    'Please choose an option by entering a number:',
+    '1 - List all books',
+    '2 - List all people',
+    '3 - Create a person',
+    '4 - Create a book',
+    '5 - Create a rental',
+    '6 - List all rentals for a given person id',
+    '7 - Exit'
+  ]
+end
 
+def main
+  app = App.new
+  puts 'Welcome to School Library App.'
+
+  app_should_run = true
+  while app_should_run
+    puts options
+    user_selection = gets.to_i
+    case user_selection
+    when 1 then list_books(app.books)
+    when 2 then list_people(app.people)
+    when 3 then create_person(app)
+    when 4 then create_book(app)
+    when 5 then create_rental(app)
+    when 6 then list_rentals(app)
+    when 7 then app_should_run = false
+    else puts 'Please input a number between 1 and 7'
+    end
+  end
+end
 
 main
